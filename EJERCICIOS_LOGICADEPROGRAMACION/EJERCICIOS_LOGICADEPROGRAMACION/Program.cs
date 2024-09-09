84d892b9781147fc22ff3292d97a7821f1bbe7cf
brayan usme <brayanelgamer2@gmail.com>
2024-09-09 17:52:17 -0500

    CREACION DEL PROYECTO PARA RESOLVER EL EJERCICIO 1 DE COORDENADAS

---
+++ EJERCICIOS_LOGICADEPROGRAMACION/EJERCICIOS_LOGICADEPROGRAMACION/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+//AQUI VAMOS A TIRAR CODIGO - Lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos
