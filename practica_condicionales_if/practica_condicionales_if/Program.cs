779e7bae0ec4c334c0d61f7f90c4aefac478a649
brayan usme <brayanelgamer2@gmail.com>
2024-09-11 14:00:16 -0500

    actualizacion de practicas de condicionales

--- practica_condicionales_if/practica_condicionales_if/Program.cs
+++ practica_condicionales_if/practica_condicionales_if/Program.cs
@@ -6,14 +6,30 @@
 
 
 
-        string camello;
-
        
-        Console.WriteLine(" ingrese el valor de el camello ");
-        camello = Console.ReadLine();
 
-        Console.Write("el valora asignado es :" + camello);
 
+        int a ;
+        Console.WriteLine("ingrese el valor de a");
+       a=Convert.ToInt32(Console.ReadLine());
+
+        if (a == 1)
+        {
+            Console.WriteLine(" a vale 1");
+
+
+        }
+        else if (a == 2)
+        {
+            Console.WriteLine("a vale 2");
+        }
+
+        else {
+
+            Console.WriteLine("a no vale una monda");
+        }
+
+        Console.Write(" fin del programa");
        
 
 
