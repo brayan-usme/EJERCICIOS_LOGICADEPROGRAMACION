5a0a312da13bb97447eb7ef610e26b0bd3267499
brayan usme <brayanelgamer2@gmail.com>
2024-09-11 13:35:52 -0500

    primer avanze sobre practicas

---
+++ practica_condicionales_if/practica_condicionales_if/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+
+
+        string camello;
+
+       
+        Console.WriteLine(" ingrese el valor de el camello ");
+        camello = Console.ReadLine();
+
+        Console.Write("el valora asignado es :" + camello);
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
