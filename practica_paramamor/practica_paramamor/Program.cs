c3f0606e35b69a5ba6fa287d1c666787365f0b6d
brayan usme <brayanelgamer2@gmail.com>
2024-09-11 23:00:41 -0500

    actualizacion codigo para mamor

---
+++ practica_paramamor/practica_paramamor/Program.cs
@@ -0,0 +1,61 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        // hacer un algoritmo donde fernanda  escoja un valor de cuanto quiere a brayan del 1 al 10  y dependiendo  de cuanto lo quiera le muestre un mensaje 
+
+
+        Console.Write(" del uno al 10 cuanto amas a brayan ????");
+        double amor = Convert.ToDouble(Console.ReadLine());
+
+        if (amor < 5)
+        {
+            Console.Write(" pero por que no amaaaaaaaaaaas a tu cosita cuidadaoraaaaaaaaaaaaaaa");
+
+        }
+        else if (amor <= 7)
+        {
+            Console.Write(" jmmmmm eso esta muy suave 😭 ");
+
+        }
+        else if (amor > 9 && amor<10)
+        {
+            Console.Write(" siiiiiiiiiiii cosita muy amadaaaaaaaaaaaa <3");
+
+        }
+
+        if (amor >10 )
+        {
+            Console.WriteLine(" tu amorrrrrrrr es nivel supremooooooooooooooooo");
+
+        }
+
+
+        else
+        {
+            Console.Write("amame mucho maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaassssssssss");
+        }
+
+
+        Console.Write(" Gracias por amarme cosita , perdoname por hacerte tanto daño pero de verdad quiero volver a hacerte muy feliz como antes y que volvamos a brillar juntos ¿¿ aceptas ?? <3 ");    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
