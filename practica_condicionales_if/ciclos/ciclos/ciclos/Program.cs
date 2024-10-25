bf3ccb16f44eb887ae300e1b25bb83e870348b8a
brayan usme <brayanelgamer2@gmail.com>
2024-10-25 16:25:49 -0500

    aa

--- practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
+++ practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
@@ -8,22 +8,78 @@
         double num1 = Convert.ToDouble(Console.ReadLine());
         Console.Write(" ingrese el segundo  número ");
         double num2 = Convert.ToDouble(Console.ReadLine());
-        double opcion_menu;
+        double opcion_menu, resultado;
         bool continuar = true;
+        string continuar2;
+
 
         do
         {
-            Console.Write(" -----------------------------------");
-            Console.Write(" -- 1 sumar                      ---");
-            Console.Write(" -- 2 restar                     ---");
-            Console.Write(" -- 3 dividir                    ---");
-            Console.Write(" -- 4 multiplicar                ---");
-            Console.Write(" -- 1 sumar                      ---");
-            Console.Write(" ---------------------------------- ");
+            Console.WriteLine(" -----------------------------------");
+            Console.WriteLine(" -- 1 sumar                      ---");
+            Console.WriteLine(" -- 2 restar                     ---");
+            Console.WriteLine("  -- 3 multiplicar               ---");
+            Console.WriteLine(" -- 0 devilver                   ---");
+            Console.WriteLine(" -----------------------------------");
+           Console.WriteLine(" elija una opcion ");
             opcion_menu= Convert.ToDouble(Console.ReadLine());
 
 
-              
+            switch (opcion_menu)
+            {
+                case 0:
+                    continuar = false;
+                    Console.WriteLine(" adios");
+                    break;
+
+                case 1:
+                    resultado = num1 + num2;
+                    Console.WriteLine($" la suma de {num1}+ {num2} es {resultado}");
+                    break;
+
+
+
+                case 2:
+                    resultado = num1 - num2;
+                    Console.WriteLine($" la resta de {num1} - {num2} es {resultado}");
+
+                    break;
+
+                case 3:
+
+                    resultado = num1 * num2;
+                    Console.WriteLine($" la multiplicacion de {num1} x {num2} es {resultado}");
+                    break;
+                default:
+                    Console.WriteLine(" opcion no valida");
+
+                    break ;
+
+
+
+
+                    Console.WriteLine(" desea continuar ?? (s/n) "); 
+                    continuar2=Console.ReadLine();
+                    if (continuar2  =="s")
+                    {
+                        continuar=true;
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }  
 
 
         } while (continuar);
