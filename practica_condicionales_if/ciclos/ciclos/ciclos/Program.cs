311133021c38a1955d3aa1e7a6c2c6c24629699d
brayan usme <brayanelgamer2@gmail.com>
2024-10-25 15:47:38 -0500

    actualizacion swict case

---
+++ practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
@@ -0,0 +1,67 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        Console.Write(" ingrese el primer número ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write(" ingrese el segundo  número ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double opcion_menu;
+        bool continuar = true;
+
+        do
+        {
+            Console.Write(" -----------------------------------");
+            Console.Write(" -- 1 sumar                      ---");
+            Console.Write(" -- 2 restar                     ---");
+            Console.Write(" -- 3 dividir                    ---");
+            Console.Write(" -- 4 multiplicar                ---");
+            Console.Write(" -- 1 sumar                      ---");
+            Console.Write(" ---------------------------------- ");
+            opcion_menu= Convert.ToDouble(Console.ReadLine());
+
+
+              
+
+
+        } while (continuar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
