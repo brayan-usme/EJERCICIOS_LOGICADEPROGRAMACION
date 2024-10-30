a29c532b92386c7fbdc2d8046831dc1bb70de590
brayan usme <brayanelgamer2@gmail.com>
2024-10-30 16:30:35 -0500

    ACTUALIZ 1.0  ACTUALIZACION MIERCOLES

--- practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
+++ practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
@@ -1,7 +1,106 @@
-﻿public class Program
+﻿using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System;
+
+public class Program
 {
     private static void Main(string[] args)
     {
+       /* 32.Se desea obtener el valor de la matrícula de un estudiante cuyo valor se calcula de
+la siguiente manera:
+• Si toma 20 o menos créditos, paga el crédito al valor normal. Si toma por encima de 20 créditos, se pagarán los créditos extras al doble de
+valor normal.
+• Si el estudiante es de estrato 1, 2 o 3 recibe los siguientes descuentos:
+-Si el estrato es 1, el descuento es del 80 %.
+- Si el estrato es 2, el descuento es del 50 %.
+- Si el estrato es 3, el descuento es del 30 %.
+Además, los estratos 1 y 2 reciben subsidio de alimentación y transporte de la
+siguiente manera:
+• Para el estrato 1, el subsidio de alimentación y transporte es igual a $200.000.
+• Para el estrato 2, el subsidio de alimentación y transporte es igual a $100.000.
+Se debe informar al usuario sobre el costo de la matrícula y el valor del subsidio.
+La aplicación debe preguntar al usuario si quiere volver al inicio o no para calcular la
+matrícula de un nuevo estudiante.
+Hacer uso de todos los elementos de programación que hemos visto hasta el
+momento, incluyendo POO(clases, propiedades, métodos)*/
+
+        //DECLARO MIS VARIABLES Y VALORES A PEDIR
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
        /* Console.Write(" ingrese el primer número ");
@@ -62,7 +161,8 @@
                     continuar2 = Console.ReadLine();
                     if (continuar2 == "n")
                     {
-                        continuar = false;
+                        continuar = false;FF
+       QRFQFQFQ
                     }
 
 
