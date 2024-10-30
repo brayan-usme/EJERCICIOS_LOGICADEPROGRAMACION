428697bcb8d6a89ec45dc19b71ed59b18882fa36
brayan usme <brayanelgamer2@gmail.com>
2024-10-30 17:10:27 -0500

    actualizacion1.3

--- practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
+++ practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
@@ -31,13 +31,35 @@ public class Program
         const decimal creditos = 1000;
         const decimal credito_extra = 2000;
 
-        Console.WriteLine("¿Cuantos creditos tomo para este semestre? ");
-        double estratro_estudiante; = Convert.ToDouble(Console.ReadLine());
-
+        Console.WriteLine("¿Que estrato eres? ");
+        double estratro_estudiante = Convert.ToDouble(Console.ReadLine());
+        //descuentos
         const double descuento_estrato1 = 80;
         const double descuento_estrato2 = 50;
         const double descuento_estrato3 = 30;
+        // subsicios para estratos 1y 2
+        const decimal subsidio_estrato1 = 2000000;
+        const decimal subsidio_estrato2 = 1000000;
 
+        
+        if (num_creditos_tomados<=20)
+        {
+
+
+
+
+
+
+
+
+
+        }
+        else
+        {
+
+
+
+        }
 
 
 
