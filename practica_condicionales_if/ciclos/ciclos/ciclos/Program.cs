a548aae263f88fb3cc8287e1d35a9f67d7d4da4a
brayan usme <brayanelgamer2@gmail.com>
2024-10-30 16:27:26 -0500

    ACTUALIZACION MIERCOLES 0.0

--- practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
+++ practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
@@ -4,7 +4,7 @@
     {
 
 
-        Console.Write(" ingrese el primer número ");
+       /* Console.Write(" ingrese el primer número ");
         double num1 = Convert.ToDouble(Console.ReadLine());
         Console.Write(" ingrese el segundo  número ");
         double num2 = Convert.ToDouble(Console.ReadLine());
@@ -21,8 +21,8 @@
             Console.WriteLine("  -- 3 multiplicar               ---");
             Console.WriteLine(" -- 0 devilver                   ---");
             Console.WriteLine(" -----------------------------------");
-           Console.WriteLine(" elija una opcion ");
-            opcion_menu= Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(" elija una opcion ");
+            opcion_menu = Convert.ToDouble(Console.ReadLine());
 
 
             switch (opcion_menu)
@@ -53,16 +53,16 @@
                 default:
                     Console.WriteLine(" opcion no valida");
 
-                    break ;
+                    break;
 
 
 
 
-                    Console.WriteLine(" desea continuar ?? (s/n) "); 
-                    continuar2=Console.ReadLine();
-                    if (continuar2  =="s")
+                    Console.WriteLine(" desea continuar ?? (s/n) ");
+                    continuar2 = Console.ReadLine();
+                    if (continuar2 == "n")
                     {
-                        continuar=true;
+                        continuar = false;
                     }
 
 
@@ -79,12 +79,12 @@
 
 
 
-            }  
+            }
 
 
-        } while (continuar);
+        } while (continuar );
 
-
+        */
 
 
 
