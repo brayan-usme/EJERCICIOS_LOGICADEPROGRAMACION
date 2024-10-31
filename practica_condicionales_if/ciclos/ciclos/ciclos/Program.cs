9459b040b8cb42932c17d67b70d2d22113b3e2aa
brayan usme <brayanelgamer2@gmail.com>
2024-10-30 19:37:12 -0500

    ACTUALIZACION 2.0

--- practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
+++ practica_condicionales_if/ciclos/ciclos/ciclos/Program.cs
@@ -42,106 +42,91 @@ public class Program
         const decimal subsidio_estrato1 = 2000000;
         const decimal subsidio_estrato2 = 1000000;
         // opereaciones para creditos menores a 20
-        decimal sumade_matricula_por_preciode_matricula= num_creditos_tomados * creditos;
+        decimal sumade_matricula_por_preciode_matricula = num_creditos_tomados * creditos;
         decimal operacion_descuento_estrato1 = sumade_matricula_por_preciode_matricula * constante_descuento_estrato1 / 100;
         decimal operacion_descuento_estrato2 = sumade_matricula_por_preciode_matricula * constante_descuento_estrato2 / 100;
-        decimal operacion_descuento_estrato3 = sumade_matricula_por_preciode_matricula *constante_descuento_estrato33 / 100;
+        decimal operacion_descuento_estrato3 = sumade_matricula_por_preciode_matricula * constante_descuento_estrato33 / 100;
         // operaciones para creditos mayores a 20
-        decimal costo_creditos_normales = 20*creditos;
+        decimal costo_creditos_normales = 20 * creditos;
         decimal creditos_extras = num_creditos_tomados - 20;
         decimal costos_creditos_extras = creditos_extras + credito_extra;
-        decimal total_matricula_creditos_extras = costo_creditos_normales +costos_creditos_extras;
+        decimal total_matricula_creditos_extras = costo_creditos_normales + costos_creditos_extras;
         ///
         decimal operacion_descuento_estrato1_credito_extra = total_matricula_creditos_extras * constante_descuento_estrato1 / 100;
         decimal operacion_descuento_estrato2_credito_extra = total_matricula_creditos_extras * constante_descuento_estrato2 / 100;
         decimal operacion_descuento_estrato3_credito_extra = total_matricula_creditos_extras * constante_descuento_estrato33 / 100;
         // parte para que el programa se repita 
-        bool repetir = true;
+        int repetir;
 
         // desarrollo
 
         do
         {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        } while (repetir == true);
-
-
-
-
-
-
-        if (num_creditos_tomados <= 20)
-        {
-            if (estratro_estudiante == 1)
+            if (num_creditos_tomados <= 20)
             {
-                Console.WriteLine($"El valor de tu matricula es ${sumade_matricula_por_preciode_matricula} pero con el descuento del 80% queda en ${sumade_matricula_por_preciode_matricula - operacion_descuento_estrato1}\n adicionalmente tienes un subsidio de alimentación y transporte de : ${subsidio_estrato1}");
+                if (estratro_estudiante == 1)
+                {
+                    Console.WriteLine($"El valor de tu matricula es ${sumade_matricula_por_preciode_matricula} pero con el descuento del 80% queda en ${sumade_matricula_por_preciode_matricula - operacion_descuento_estrato1}\n adicionalmente tienes un subsidio de alimentación y transporte de : ${subsidio_estrato1}");
 
 
 
-            }
-            else if (estratro_estudiante == 2)
-            {
-                Console.WriteLine($"El valor de tu matricula es ${sumade_matricula_por_preciode_matricula} pero con el descuento del 30% queda en ${sumade_matricula_por_preciode_matricula - operacion_descuento_estrato2}\n adicionalmente tienes un subsidio de alimentación y transporte de : ${subsidio_estrato2}");
+                }
+                else if (estratro_estudiante == 2)
+                {
+                    Console.WriteLine($"El valor de tu matricula es ${sumade_matricula_por_preciode_matricula} pero con el descuento del 30% queda en ${sumade_matricula_por_preciode_matricula - operacion_descuento_estrato2}\n adicionalmente tienes un subsidio de alimentación y transporte de : ${subsidio_estrato2}");
 
-            }
-            else if (estratro_estudiante == 3)
-            {
-                Console.WriteLine($"El valor de tu matricula es ${sumade_matricula_por_preciode_matricula} pero con el descuento del 30% queda en ${sumade_matricula_por_preciode_matricula - operacion_descuento_estrato3}\n No tienes ningun tipo de subsidio");
-            }
-            else {
-                Console.WriteLine(" Tu estrato social no esta permitido ");
-            }
-
-
-
+                }
+                else if (estratro_estudiante == 3)
+                {
+                    Console.WriteLine($"El valor de tu matricula es ${sumade_matricula_por_preciode_matricula} pero con el descuento del 30% queda en ${sumade_matricula_por_preciode_matricula - operacion_descuento_estrato3}\n No tienes ningun tipo de subsidio");
+                }
+                else
+                {
+                    Console.WriteLine(" Tu estrato social no esta permitido ");
+                }
 
 
 
-        }
-        else
-        {
-            if (estratro_estudiante == 1)
-            {
-                Console.WriteLine($"El valor de tu matricula es ${total_matricula_creditos_extras} pero con el descuento del 80% queda en ${total_matricula_creditos_extras - operacion_descuento_estrato1_credito_extra}\n adicionalmente tienes un subsidio de alimentación y transporte de : ${subsidio_estrato1}");
 
 
 
-            }
-            else if (estratro_estudiante == 2)
-            {
-
-                Console.WriteLine($"El valor de tu matricula es ${total_matricula_creditos_extras} pero con el descuento del 50% queda en %{total_matricula_creditos_extras - operacion_descuento_estrato2_credito_extra}\n adicionalmente tienes un subsidio de alimentación y transporte de : ${subsidio_estrato2}");
-            }
-            else if (estratro_estudiante == 3)
-            {
-                Console.WriteLine($"El valor de tu matricula es ${total_matricula_creditos_extras} pero con el descuento del 30% queda en ${total_matricula_creditos_extras - operacion_descuento_estrato3_credito_extra}\n No tienes ningun tipo de subsidio");
             }
             else
             {
-                Console.WriteLine(" Tu estrato social no esta permitido ");
+                if (estratro_estudiante == 1)
+                {
+                    Console.WriteLine($"El valor de tu matricula es ${total_matricula_creditos_extras} pero con el descuento del 80% queda en ${total_matricula_creditos_extras - operacion_descuento_estrato1_credito_extra}\n adicionalmente tienes un subsidio de alimentación y transporte de : ${subsidio_estrato1}");
+
+
+
+                }
+                else if (estratro_estudiante == 2)
+                {
+
+                    Console.WriteLine($"El valor de tu matricula es ${total_matricula_creditos_extras} pero con el descuento del 50% queda en %{total_matricula_creditos_extras - operacion_descuento_estrato2_credito_extra}\n adicionalmente tienes un subsidio de alimentación y transporte de : ${subsidio_estrato2}");
+                }
+                else if (estratro_estudiante == 3)
+                {
+                    Console.WriteLine($"El valor de tu matricula es ${total_matricula_creditos_extras} pero con el descuento del 30% queda en ${total_matricula_creditos_extras - operacion_descuento_estrato3_credito_extra}\n No tienes ningun tipo de subsidio");
+                }
+                else
+                {
+                    Console.WriteLine(" Tu estrato social no esta permitido ");
+                }
+
+
             }
+            Console.WriteLine("SI DESEAS VOLVER A CALCULAR PRESIONA ( 1 ) DE LO CONTRATIO PRESIONA ( ENTER ) PARA FINALIZAR EL PROGRAMA   \n  ");
+            repetir = Convert.ToInt32(Console.ReadLine());
+        
+
+        } while (repetir==1);
+
+        
 
 
-        }
+
+
 
 
 
